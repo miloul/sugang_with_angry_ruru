41e194ff2588f9e58749b8e5fe9726d491d3ef04
ruru <milouw@naver.com>
2021-08-19 01:44:56 +0900

    버튼 이벤트 몇개

--- js/firstp.js
+++ js/firstp.js
@@ -1,47 +1,49 @@
 //방만들기 화면
-const checkClock = document.querySelector("#check_clock");
-const result_c = document.querySelector("#result_c");
-const checkLogin = document.querySelector("#check_login");
-const result_l = document.querySelector("#result_l");
 const inviteButton = document.querySelector("button.invite");
-
 const readyButton = document.querySelector(".ready");
 const startButton = document.querySelector(".start");
-const nameButton = document.querySelector(".ok");
-const member_pButton = document.querySelector(".p");
-const member_mButton = document.querySelector(".m");
+const nameButton = document.querySelector("#ok");
 const makeButton= document.querySelector(".make");
 const getsubButton = document.querySelector(".get_sub");
 
 const clock_DoneButton = document.querySelector(".time_submit");
 const clock_ModifyButton = document.querySelector(".time_modify");
-const login_DoneButton = document.querySelector(".id_submit");
-const login_ModifyButton = document.querySelector(".id_modify");
 
-const countMember = document.querySelector(".count_member");
-const allMember = document.querySelector(".all_member");
+let countMember = document.querySelector(".count_member");
 
-member_pButton.addEventListener('click', plusMember);
-member_nButton.addEventListener('click', minusMember);
+// +, - 버튼
+document.getElementById("p").addEventListener('click', plus);
+function plus() {
+    let member=document.getElementById("all_member").innerText;
+    let member_int=Number(member);
+    
+    member_int=member_int+1;
+    const allMember = document.querySelector("#all_member");
+    allMember.innerText=member_int;
+}
 
-function plusMember() {
-    let allMember_int=Number(allMember.value);
-    allMember.innerText=allMember_int;
-
+document.getElementById("m").addEventListener('click', minus);
+function minus() {
+    let member=document.getElementById("all_member").innerText;
+    let member_int=Number(member);
+    
+    if (member_int>1) {
+        member_int=member_int-1;
+    }
+    
+    const allMember = document.querySelector("#all_member");
+    allMember.innerText=member_int;
 }
 
 console.log(readyButton);
 console.log(startButton);
 console.log(nameButton);
-console.log(member_pButton);
-console.log(member_mButton);
 console.log(makeButton);
 console.log(getsubButton);
 console.log(clock_DoneButton);
 console.log(clock_ModifyButton);
-console.log(login_DoneButton);
-console.log(login_ModifyButton);
 
+// 초대링크 버튼
 inviteButton.addEventListener('click', copyUrl);
 function copyUrl() {
     let url = window.location.href;
@@ -56,21 +58,22 @@ function copyUrl() {
     alert("복사되었습니다");
 }
 
-/*result_c.style.display="none";
-checkClock.addEventListener('change', function(e){
-    if (checkClock.checked){
-        result_c.style.display="initial";
+//닉네임 설정완료 버튼
+nameButton.addEventListener('click', submit);
+function submit() {
+    const name=document.getElementById("name_txt");
+    console.log(name);
+    if (name.value==""){
+        alert("닉네임을 입력하세요");
     }
     else {
-        result_c.style.display="none";
+        name.disabled=true;
+        alert("설정완료");
     }
-});*/
-result_l.style.display="none";
-checkLogin.addEventListener('change', function(e){
-    if (checkLogin.checked){
-        result_l.style.display="initial";
-    }
-    else {
-        result_l.style.display="none";
-    }
-});
+}
+
+//과목설정 - 직접 만들기
+makeButton.addEventListener('click', makeSub);
+function makeSub() {
+    
+}
